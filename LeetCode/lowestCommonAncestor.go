5c9ec7d00ad56d691be2dafcd8d3d14dbcff35bf
honubin <honubin1@linefriends.com>
2024-01-25 17:59:57 +0900

    feat: Add problems

--- LeetCode/lowestCommonAncestor.go
+++ LeetCode/lowestCommonAncestor.go
@@ -1,78 +1,86 @@
 package main
 
 import (
-	"strconv"
+	"fmt"
 )
 
 // Definition for a binary tree node.
 type TreeNode struct {
-   	Val int
-   	Left *TreeNode
-   	Right *TreeNode
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
-// type Stack []*TreeNode
+type Stack []*TreeNode
 
-// func (s *Stack) IsEmpty() bool {
-// 	return len(*s) == 0
-// }
+func (s *Stack) IsEmpty() bool {
+	return len(*s) == 0
+}
 
-// func (s *Stack) Push(node *TreeNode) {
-// 	*s = append(*s, node)
-// }
+func (s *Stack) Push(node *TreeNode) {
+	*s = append(*s, node)
+}
 
-// func (s *Stack) Pop() *TreeNode {
-// 	if s.IsEmpty() {
-// 		fmt.Println("empty")
-// 		return nil
-// 	}
-// 	top := len(*s)-1
-// 	node := (*s)[top]
-// 	*s = (*s)[:top]
+func (s *Stack) Pop() *TreeNode {
+	if s.IsEmpty() {
+		fmt.Println("empty")
+		return nil
+	}
+	top := len(*s) - 1
+	node := (*s)[top]
+	*s = (*s)[:top]
 
-// 	return node
-// }
+	return node
+}
 
 var count int
-var parentsStack = make([]string, 2)
-var nodeMap = make(map[int]*TreeNode)
+var parentsStack []Stack
+var stack Stack
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	makeParentsStack("", root, p, q)
-	// fmt.Println(parentsStack)
+	count = 0
+	parentsStack = make([]Stack, 2)
+	stack = Stack{}
+	makeParentsStack(root, p, q, stack)
 
 	return findLCA(parentsStack[0], parentsStack[1])
 }
 
-func makeParentsStack(stack string, node, p, q *TreeNode) {
-	nodeMap[node.Val] = node
-	stack = stack + strconv.Itoa(node.Val)
+func makeParentsStack(node, p, q *TreeNode, stack Stack) {
+	stack.Push(node)
 	if node.Val == p.Val || node.Val == q.Val {
-		parentsStack[count] = stack
+		copiedStack := make([]*TreeNode, len(stack))
+		copy(copiedStack, stack)
+		parentsStack[count] = copiedStack
 		count++
 	}
 	if count == 2 {
 		return
 	} else {
 		if node.Left != nil {
-			makeParentsStack(stack, node.Left, p, q)
+			makeParentsStack(node.Left, p, q, stack)
 		}
 		if node.Right != nil {
-			makeParentsStack(stack, node.Right, p, q)
+			makeParentsStack(node.Right, p, q, stack)
 		}
 	}
 }
 
-func findLCA(pStack, qStack string) *TreeNode {
-	leftDepth := len(pStack)
-	rightDepth := len(qStack)
-	if leftDepth > rightDepth {
-		return findLCA(pStack[:rightDepth], qStack)
-	} else if leftDepth < rightDepth {
-		return findLCA(pStack, qStack[:leftDepth])
-	} else if pStack == qStack {
-		val, _ := strconv.Atoi(pStack[leftDepth-1:leftDepth])
-		return nodeMap[val]
+func findLCA(pStack, qStack Stack) *TreeNode {
+	pDepth := len(pStack)
+	qDepth := len(qStack)
+	if pDepth > qDepth {
+		pStack.Pop()
+		return findLCA(pStack, qStack)
+	} else if pDepth < qDepth {
+		qStack.Pop()
+		return findLCA(pStack, qStack)
+	} else {
+		p := pStack.Pop()
+		q := qStack.Pop()
+		if p == q {
+			return p
+		}
+		return findLCA(pStack, qStack)
 	}
-	return findLCA(pStack[:rightDepth-1], qStack[:leftDepth-1])
 }
--- LeetCode/main.go
+++ LeetCode/main.go
@@ -5,34 +5,25 @@ import "fmt"
 func main() {
 	fmt.Println(lowestCommonAncestor(
 		&TreeNode{
-		Val: 3,
-		Left: &TreeNode{
-			Val: 5,
+			Val: -1,
 			Left: &TreeNode{
-				Val: 6,
-			},
-			Right: &TreeNode{
-				Val: 2,
+				Val: 0,
 				Left: &TreeNode{
-					Val: 7,
+					Val: -2,
+					Left: &TreeNode{
+						Val: 8,
+					},
 				},
 				Right: &TreeNode{
 					Val: 4,
 				},
 			},
-		},
-		Right: &TreeNode{
-			Val: 1,
-			Left: &TreeNode{
-				Val: 0,
-			},
 			Right: &TreeNode{
-				Val: 8,
+				Val: 3,
 			},
-		},
-	}, &TreeNode{
-		Val: 5,
-	}, &TreeNode{
-		Val: 1,
-	}))
+		}, &TreeNode{
+			Val: 3,
+		}, &TreeNode{
+			Val: 8,
+		}))
 }
